13f7b594106ed8c471d0b695a20e03be3e6715df
babu <babu@debABU>
2020-07-01 15:52:27 -0400

    basic working todo

--- src/App.js
+++ src/App.js
@@ -3,33 +3,28 @@ import {useState} from 'react'
 
 import './App.css';
 
+
+class ToDo {
+  constructor(text, completed) {
+    this.content = text;
+    this.completed = completed
+  }
+  empty() {
+    return this.content.length===0
+  }
+}
+
+
+
 function App() {
 
-  
-  const initToDoItems = [
-    { content: "stuff",
-    completed: false,
-    hidden: function() {
-      return this.content.length === 0
-    }
-  },
-    { content: "stuff2",
-    completed: false,
-    hidden: function() {
-      return this.content.length === 0
-    }
-  },
-    { content: "",
-    completed: true,
-    hidden: function() {
-      return this.content.length === 0
-    }
-  }
-    ]
 
-  
+  var item1 = new ToDo("stuff", false)
+  var item2 = new ToDo("stuff also", false)
+  var item3 = new ToDo("finished this one", true)
+  var item4 = new ToDo("", false)
 
-
+  const initToDoItems = [item1, item2, item3, item4]
   const [toDoItems, setToDoItems] = useState(initToDoItems)
 
   const toggleCompleted = (indexToDo) => {
@@ -42,19 +37,40 @@ function App() {
     else {
       _todo[indexToDo].completed = true
     }
-    
-
+    // const newToDoItem = new ToDo("", false);
+    // _todo.push(newToDoItem)
     setToDoItems(_todo)
-
   }
 
   const updateToDo = (event, indexToDo)  => {
 
+    
     const _todo = [...toDoItems]
     _todo[indexToDo].content = event.target.value
-
+    // if (_todo.length-1 === indexToDo && event.target.value.length===0) {
+    //   const newToDoItem = new ToDo("", false);
+    //   _todo.push(newToDoItem)
+    // }
     setToDoItems(_todo)
+  }
 
+  const keyDown = (event, indexToDo) => {
+    //backspace
+    if( event.keyCode === 8 && toDoItems[indexToDo].content.length===0){
+      const _todo = [...toDoItems]
+      _todo.splice(indexToDo, 1)
+      setToDoItems(_todo)
+      console.log("backspace!!!")
+    }
+
+    if( event.keyCode === 13 && toDoItems[indexToDo].content.length>=0){
+      const _todo = [...toDoItems]
+      _todo.splice(indexToDo+1, 0, new ToDo("", false))
+      setToDoItems(_todo)
+      console.log("Enter!!!")
+    }
+
+    
   }
 
   return (
@@ -63,8 +79,11 @@ function App() {
       {toDoItems.map((todo, index)=> {
         return (
         <li key={index}>
-          <input type="checkbox" onChange={(event) => toggleCompleted(index)} checked={todo.completed} hidden={todo.hidden()}/>
-          <input type="text" onChange={(event) => updateToDo(event, index) } value={todo.content}/>
+          <input type="checkbox" onChange={(event) => toggleCompleted(index)} checked={todo.completed} hidden={todo.empty()}/>
+          <input type="text" placeholder="Enter item"
+          onKeyDown={(event) => keyDown(event, index)} 
+          onChange={(event) => updateToDo(event, index) } 
+          value={todo.content}/>
         </li>
       )
       })}
