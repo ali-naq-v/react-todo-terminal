42ae3664ab7fbdab615e8751b1d713de56c5108f
babu <babu@debABU>
2020-07-02 14:45:52 -0400

    cleaned up the code

--- src/App.js
+++ src/App.js
@@ -25,16 +25,11 @@ class ToDo {
 function App() {
   
   var item1 = new ToDo("", false)
-  // var item2 = new ToDo("stuff also", false)
-  // var item3 = new ToDo("finished this one", true)
-  // var item4 = new ToDo("", false)
-  
-  //const initToDoItems = [item1, item2, item3, item4]
   const initToDoItems = [item1]
   const [toDoItems, setToDoItems] = useState(initToDoItems)
   const refsArray = useRef([])
 
-  const toggleCompleted = (indexToDo) => {
+  function toggleCompleted(indexToDo) {
 
     const _todo = [...toDoItems]
     if (_todo[indexToDo].completed) {
@@ -46,47 +41,65 @@ function App() {
     setToDoItems(_todo)
   }
 
-  const updateToDo = (event, indexToDo)  => {
+  function updateToDo(event, indexToDo) {
 
     const _todo = [...toDoItems]
     _todo[indexToDo].content = event.target.value
     setToDoItems(_todo)
   }
 
-  const keyDown = (event, indexToDo) => {
-    //up arrow
-    if (event.keyCode === 38) {
+  function keyDown(event, indexToDo) {
 
-      if (indexToDo> 0) {
+    upArrowKeyDown(event, indexToDo)
+    downArrowKeyDown(event, indexToDo)
+    backSpaceKeyDown(event, indexToDo)
+    enterKeyDown(event, indexToDo)
+  }
+
+
+  function upArrowKeyDown(event, indexToDo) {
+
+    if (event.keyCode !== 38) return;
+    
+    if (indexToDo> 0) {
       setTimeout(() => {
         refsArray.current[indexToDo-1].focus()
       }, 0);
     }
+  }
 
-    }
-    //down arrow
-    if (event.keyCode === 40) {
+  function downArrowKeyDown(event, indexToDo) {
 
-      if (toDoItems.length-1 !== indexToDo) {
+    if (event.keyCode !== 40) return;
+  
+    if (toDoItems.length-1 !== indexToDo) {
       setTimeout(() => {
         refsArray.current[indexToDo+1].focus()
       }, 0);
     }
+  }
 
-    }
+  function backSpaceKeyDown(event, indexToDo) {
 
-    //backspace
-    if( event.keyCode === 8 && toDoItems[indexToDo].content.length===0){
+    if( event.keyCode !== 8) return;
+
+    if (toDoItems.length === 1) return;
+ 
+    if (toDoItems[indexToDo].content.length===0){
       const _todo = [...toDoItems]
       _todo.splice(indexToDo, 1)
-      
       setTimeout(() => {
         setToDoItems(_todo)
         refsArray.current[indexToDo-1].focus()
       }, 100);
     }
-    //Enter key
-    if( event.keyCode === 13 && toDoItems[indexToDo].content.length>=0){
+  }
+
+  function enterKeyDown(event, indexToDo) {
+    
+    if( event.keyCode !== 13 ) return;
+
+    if (toDoItems[indexToDo].content.length>=0){
       const _todo = [...toDoItems]
       _todo.splice(indexToDo+1, 0, new ToDo("", false))
       
