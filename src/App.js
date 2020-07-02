245534d127d73ab4e238264e335cb61e9eab8ace
babu <babu@debABU>
2020-07-02 01:24:11 -0400

    added some cool styling

--- src/App.js
+++ src/App.js
@@ -24,12 +24,13 @@ class ToDo {
 
 function App() {
   
-  var item1 = new ToDo("stuff", false)
-  var item2 = new ToDo("stuff also", false)
-  var item3 = new ToDo("finished this one", true)
-  var item4 = new ToDo("", false)
+  var item1 = new ToDo("", false)
+  // var item2 = new ToDo("stuff also", false)
+  // var item3 = new ToDo("finished this one", true)
+  // var item4 = new ToDo("", false)
   
-  const initToDoItems = [item1, item2, item3, item4]
+  //const initToDoItems = [item1, item2, item3, item4]
+  const initToDoItems = [item1]
   const [toDoItems, setToDoItems] = useState(initToDoItems)
   const refsArray = useRef([])
 
@@ -53,24 +54,46 @@ function App() {
   }
 
   const keyDown = (event, indexToDo) => {
+    //up arrow
+    if (event.keyCode === 38) {
+
+      if (indexToDo> 0) {
+      setTimeout(() => {
+        refsArray.current[indexToDo-1].focus()
+      }, 0);
+    }
+
+    }
+    //down arrow
+    if (event.keyCode === 40) {
+
+      if (toDoItems.length-1 !== indexToDo) {
+      setTimeout(() => {
+        refsArray.current[indexToDo+1].focus()
+      }, 0);
+    }
+
+    }
 
     //backspace
     if( event.keyCode === 8 && toDoItems[indexToDo].content.length===0){
       const _todo = [...toDoItems]
       _todo.splice(indexToDo, 1)
-      setToDoItems(_todo)
+      
       setTimeout(() => {
+        setToDoItems(_todo)
         refsArray.current[indexToDo-1].focus()
-      }, 0);
+      }, 100);
     }
     //Enter key
     if( event.keyCode === 13 && toDoItems[indexToDo].content.length>=0){
       const _todo = [...toDoItems]
       _todo.splice(indexToDo+1, 0, new ToDo("", false))
-      setToDoItems(_todo)
+      
       setTimeout(() => {
+        setToDoItems(_todo)
         refsArray.current[indexToDo+1].focus()
-      }, 0);
+      }, 100);
     }
   }
 
